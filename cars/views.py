ea26ee05167f02665c4e7e75efed62f35cc307bc
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-03-29 18:15:26 -0400

    Feat: add variaveis dinamicas template

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from cars.models import Car
 
 def cars_view(request):
+    cars = Car.objects.all() # buscando dados do banco de dados
+
     return render(
         request,
         'cars.html',
-        {'cars':{'model': 'astra 2.0'}}
+        {'cars': cars}
     )
