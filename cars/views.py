7dd98301d8bdea5d8ce0a0e423ea20cbdafba54c
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-03-31 18:40:36 -0400

    feat: add templates

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all() # buscando dados do banco de dados
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__icontains=search)
 
     return render(
         request,
