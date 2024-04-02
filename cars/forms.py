17868c794fb9503f48a38dcd1e598d7b257a47b4
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-04-02 14:34:38 -0400

    feat: migrando para ModelForm

--- cars/forms.py
+++ cars/forms.py
@@ -2,26 +2,31 @@ from django import forms
 from cars.models import Brand, Car
 
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField()
-    value = forms.CharField(max_length=10)
-    photo = forms.ImageField()
+# class CarForm(forms.Form):
+#     model = forms.CharField(max_length=200)
+#     brand = forms.ModelChoiceField(Brand.objects.all())
+#     factory_year = forms.IntegerField()
+#     model_year = forms.IntegerField()
+#     plate = forms.CharField()
+#     value = forms.CharField(max_length=10)
+#     photo = forms.ImageField()
 
-    def save(self):
-        car = Car(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            value = self.cleaned_data['value'],
-            photo = self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
+#     def save(self):
+#         car = Car(
+#             model = self.cleaned_data['model'],
+#             brand = self.cleaned_data['brand'],
+#             factory_year = self.cleaned_data['factory_year'],
+#             model_year = self.cleaned_data['model_year'],
+#             plate = self.cleaned_data['plate'],
+#             value = self.cleaned_data['value'],
+#             photo = self.cleaned_data['photo'],
+#         )
+#         car.save()
+#         return car
 
-#class CarModelForm(forms.ModelForm):
+#---- todas essas linhas acima, podem ser substituidas pelas de baixo.
+
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
